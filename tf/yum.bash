a7fceb3ffa00c2d812e0db9c02a489f32eb9433f
betocatalan <150401818+betocatalan@users.noreply.github.com>
2023-11-14 01:56:11 -0600

    Update yum.bash 1

--- tf/yum.bash
+++ tf/yum.bash
@@ -1,15 +1,48 @@
 #!/bin/bash
-sudo apt update
+
+
+## Install Docker 
+sudo apt-get update
 sudo apt-get install -y docker.io
-sudo apt-get install -y docker-compose 
+sudo apt-get install -y docker-compose
+
+
 cd /home/ubuntu
 mkdir app
+
+
+## Create docker-file
 echo "version: '3.3'
+
 services:
   python-api:
     image: cataluniat1/django1:latest
     ports:
       - "80:80"
     restart: always
-" >> ./app/docker-compose.yml 
-sudo docker-compose -f ./app/docker-compose.yml up -d 
+" >> ./app/docker-compose.yml
+
+
+## Create docker boot service
+echo "[Unit]
+Description=docker boot
+After=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+WorkingDirectory=/home/ubuntu/app
+ExecStartPre=sudo docker-compose -f /home/ubuntu/app/docker-compose.yml pull
+ExecStart=sudo docker-compose -f /home/ubuntu/app/docker-compose.yml up -d
+
+[Install]
+WantedBy=multi-user.target
+" >> /etc/systemd/system/docker_boot.service
+
+
+## Enable and start docker boot service
+sudo systemctl enable docker_boot.service
+sudo systemctl start docker_boot.service
+
+
+sudo docker-compose -f /home/ubuntu/app/docker-compose.yml up -d
